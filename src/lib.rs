520017952e771d923358a96764b8e0ab4ba9ab24
Jeremy Fitzhardinge <jeremy@goop.org>
2015-07-09 21:14:30 -0700

    Example update

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,21 @@
 //! The top-level type is `Histogram`.
 //!
 //! # Example
+//!
+//! This sets up a histogram to record values in the range 1..1000_000 with 2 significant figures of
+//! precision. It then records 
+//!
 //! ```
 //! # use hdrhistogram::Histogram;
 //! let mut h = Histogram::init(1, 1000000, 2).unwrap();
 //! 
 //! h.record_value(1);
 //! h.record_value(10);
+//! h.record_value(100, 40);
 //!
-//! assert_eq!(h.total_count(), 2);
+//! assert_eq!(h.total_count(), 42);
 //! assert_eq!(h.min(), 1);
-//! assert_eq!(h.max(), 10);
+//! assert_eq!(h.max(), 100);
 //! ```
 
 mod ffi;
