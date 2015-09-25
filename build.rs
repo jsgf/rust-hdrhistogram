90babab419fb00348f505024b2bad8a88b36763d
Jeremy Fitzhardinge <jeremy@goop.org>
2015-09-25 14:28:16 -0700

    Make sure we use gnu99 standard.

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 extern crate cmake;
 
+use cmake::Config;
+
 fn main() {
     // Builds the project in the directory located in `libfoo`, installing it
     // into $OUT_DIR
-    let dst = cmake::build("HdrHistogram_c/src");
+    let dst = Config::new("HdrHistogram_c/src")
+        .cflag("-std=gnu99")
+        .build();
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static=hdr_histogram_static");
